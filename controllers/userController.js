80fb606c520ed75342c3231dca4b426a07b52a61
Alan Nieto <110429020+alannieto07@users.noreply.github.com>
2024-02-10 17:31:10 -0300

    terminamos la api rest

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,4 @@
+
 const user = require('../models/user');
 
 exports.createUser = async (req,res) =>{
@@ -20,6 +21,62 @@ exports.getAllUsers = async (req,res) =>{
         res.status(500).send('error interno del servidor');
     }
 };
+
+exports.GetUserByID = async (req,res) =>{
+
+    try{
+        const userByid = await user.findByPk(req.params.id)
+        if(!userByid){
+            res.status(400).send("usuario no econtrado");
+        }else{
+            res.json(userByid)
+        }
+    }catch(error){
+        console.log(error)
+        res.status(500).send("error interno del servidor")
+    };
+    
+};
+
+exports.updateUser = async (req,res) => {
+
+try{
+    const userUpdate = await user.findByPk(req.params.id);
+    if(!userUpdate){
+        res.status(400).send("usuario no encontrado")
+    } else {
+        await userUpdate.update(req.body);
+        res.json(userUpdate)
+        }
+        }catch(error){
+        console.log(error)
+        res.status(500).send("erorr en el servidor")
+        }
+
+}
+
+exports.DeleteByID = async (req,res) =>{
+
+try {
+    const DeleteUser = await user.findByPk(req.params.id);
+    if(!DeleteUser){
+        res.status(400).send("usuario no encontrado");
+    }else {
+        await DeleteUser.destroy();
+        res.send("usuario eliminado con exito")
+    }
+    }catch(error){
+        res.status(500).send("error interno en el servidor");
+    };
+
+};
+
+
+
+
+
+
+
 
 
 
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,7 +4,9 @@ const userController = require('../controllers/userController');
 
 router.post('/', userController.createUser);
 router.get('/', userController.getAllUsers);
-
+router.get('/:id', userController.GetUserByID);
+router.put('/:id', userController.updateUser);
+router.delete('/:id', userController.DeleteByID);
 
 
 module.exports = router;
